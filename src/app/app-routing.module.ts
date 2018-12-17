49c836d6f71b40f1b4ddfae78f42788744522bf3
Trent <mr.peschke@gmail.com>
2018-12-17 12:25:56 -0700

    debugged routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { ChapterComponent } from './chapter/chapter.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'chapter/c0', component: HomeComponent},
   {path: 'chapter/c16', component: HomeComponent},
   {path: 'chapter/:id', component: ChapterComponent}
