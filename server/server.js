9279ad6edf5d8bbe2abd9bdaf7237b64d4beb63d
Trent <mr.peschke@gmail.com>
2018-12-17 11:19:52 -0700

    hosting debugging

--- server/server.js
+++ server/server.js
@@ -14,19 +14,19 @@ app.use( express.static( __dirname + `/../dist/bonfireSRD` ) );
 
 app.get('/chapter/c1', ctrl.c1);
 app.get('/chapter/c2', ctrl.c2);
-app.get('/chapter/c3', ctrl.c3);
-app.get('/chapter/c4', ctrl.c4);
-app.get('/chapter/c5', ctrl.c5);
-app.get('/chapter/c6', ctrl.c6);
-app.get('/chapter/c7', ctrl.c7);
-app.get('/chapter/c8', ctrl.c8);
-app.get('/chapter/c9', ctrl.c9);
-app.get('/chapter/c10', ctrl.c10);
-app.get('/chapter/c11', ctrl.c11);
-app.get('/chapter/c12', ctrl.c12);
-app.get('/chapter/c13', ctrl.c13);
-app.get('/chapter/c14', ctrl.c14);
-app.get('/chapter/c15', ctrl.c15);
+app.get('/c/c3', ctrl.c3);
+app.get('/c/c4', ctrl.c4);
+app.get('/c/c5', ctrl.c5);
+app.get('/c/c6', ctrl.c6);
+app.get('/c/c7', ctrl.c7);
+app.get('/c/c8', ctrl.c8);
+app.get('/c/c9', ctrl.c9);
+app.get('/c/c10', ctrl.c10);
+app.get('/c/c11', ctrl.c11);
+app.get('/c/c12', ctrl.c12);
+app.get('/c/c13', ctrl.c13);
+app.get('/c/c14', ctrl.c14);
+app.get('/c/c15', ctrl.c15);
 
 // ================================== \\
 
