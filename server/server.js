3d6809db666ca22c45d9f138be8e9cc56b7da25d
Trent <mr.peschke@gmail.com>
2018-12-19 09:17:43 -0700

    debugging cron

--- server/server.js
+++ server/server.js
@@ -13,10 +13,26 @@ app.use(cors())
 app.use( express.static( __dirname + `/../dist/bonfireSRD` ) );
 
 new CronJob('0 0 0 * * *', _ => {
-    let d = new Date()
-    console.log('I ran!', d.getDay())
     ctrl.forceRun({body: {auth}}, null)
-})
+}, null, true, 'America/Los_Angeles');
+
+///////////////////////////////////
+////TESTING TOPLEVEL MIDDLEWARE////
+///COMMENET OUT WHEN AUTH0 READY///
+///////////////////////////////////
+// app.use((req, res, next) =>{
+//     if(!req.session.user){
+//         req.session.user = {
+//             user_id: 1,
+//             user_name: "harrison ford", 
+//             email: "adventureBuilder2049@gmail.com", 
+//             name: "adventure", 
+//             profile_picture : "http://www.placekitten.com/200/250",
+//             auth_id: "adsgfhaoibjmoi5wrhgiuaosfngiuasdhg;ioarhdgv;ou"
+//         }
+//     }
+//     next();
+// })
 
 // ================================== \\
 
