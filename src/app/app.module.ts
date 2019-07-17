9adbab2513cbd3e3d3bb260ea9370c784ace5acd
Trent <mr.peschke@gmail.com>
2019-07-17 09:07:48 -0600

    finished chapter 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { QuickNavComponent } from './quick-nav/quick-nav.component';
 
 import { ChapterOneComponent } from './chapter-one/chapter-one.component';
 import { ChapterTwoComponent } from './chapter-two/chapter-two.component';
+import { ChapterThreeComponent } from './chapter-three/chapter-three.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ChapterTwoComponent } from './chapter-two/chapter-two.component';
     SearchComponent,
     FormatSearchPipe,
     ChapterOneComponent,
-    ChapterTwoComponent
+    ChapterTwoComponent,
+    ChapterThreeComponent
   ],
   imports: [
     BrowserModule,
