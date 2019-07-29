1ef119de01fd213823f4b8b7f09a6a6a44494b53
Trent <mr.peschke@gmail.com>
2019-07-29 14:04:48 -0600

    patch 0.4.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ChapterOneComponent } from './chapter-one/chapter-one.component';
-import { ChapterTwoComponent } from './chapter-two/chapter-two.component'
-import { ChapterThreeComponent } from './chapter-three/chapter-three.component'
-import { ChapterFiveComponent } from './chapter-five/chapter-five.component'
 import { ChapterSixComponent } from './chapter-six/chapter-six.component'
-import { ChapterSevenComponent } from './chapter-seven/chapter-seven.component'
 import { ChapterNineComponent } from './chapter-nine/chapter-nine.component'
 import { ChapterTenComponent } from './chapter-ten/chapter-ten.component'
 import { ChapterFourteenComponent } from './chapter-fourteen/chapter-fourteen.component'
@@ -18,6 +14,11 @@ import { ChapterTwelveModule } from './chapter-twelve/chapter-twelve.module'
 import { ChapterElevenModule } from './chapter-eleven/chapter-eleven.module'
 import { ChapterThirteenModule } from './chapter-thirteen/chapter-thirteen.module'
 import { ChapterEightModule } from './chapter-eight/chapter-eight.module'
+import { ChapterFiveModule } from './chapter-five/chapter-five.module'
+import { ChapterSevenModule } from './chapter-seven/chapter-seven.module'
+import { ChapterThreeModule } from './chapter-three/chapter-three.module'
+import { ChapterTwoModule } from './chapter-two/chapter-two.module'
+import { ChapterTenModule } from './chapter-ten/chapter-ten.module'
 
 import { SelectivePreloadingStrategyService } from './selective-preloading.service'
 
@@ -32,14 +33,14 @@ const routes: Routes = [
   {path: 'chapter/13', loadChildren: () => ChapterThirteenModule, data: { preload: true } },
   {path: 'chapter/11', loadChildren: () => ChapterElevenModule, data: { preload: true } },
   {path: 'chapter/8', loadChildren: () => ChapterEightModule, data: { preload: true } },
+  {path: 'chapter/5', loadChildren: () => ChapterFiveModule, data: { preload: true } },
+  {path: 'chapter/7', loadChildren: () => ChapterSevenModule, data: { preload: true } },
+  {path: 'chapter/3', loadChildren: () => ChapterThreeModule, data: { preload: true } },
+  {path: 'chapter/2', loadChildren: () => ChapterTwoModule, data: { preload: true } },
+  {path: 'chapter/10', loadChildren: () => ChapterTenModule, data: { preload: true } },
   {path: 'chapter/1', component: ChapterOneComponent},
-  {path: 'chapter/2', component: ChapterTwoComponent},
-  {path: 'chapter/3', component: ChapterThreeComponent},
-  {path: 'chapter/5', component: ChapterFiveComponent},
   {path: 'chapter/6', component: ChapterSixComponent},
-  {path: 'chapter/7', component: ChapterSevenComponent},
   {path: 'chapter/9', component: ChapterNineComponent},
-  {path: 'chapter/10', component: ChapterTenComponent},
   {path: 'chapter/14', component: ChapterFourteenComponent},
   {path: 'chapter/15', component: ChapterFifteenComponent},
   {path: 'search', component: SearchComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,8 @@ import { FormatSearchPipe } from './format-search.pipe';
 import { QuickNavModule } from './quick-nav/quick-nav.module';
 
 import { ChapterOneComponent } from './chapter-one/chapter-one.component';
-import { ChapterTwoComponent } from './chapter-two/chapter-two.component';
-import { ChapterThreeComponent } from './chapter-three/chapter-three.component';
-import { ChapterFiveComponent } from './chapter-five/chapter-five.component';
 import { ChapterSixComponent } from './chapter-six/chapter-six.component';
-import { ChapterSevenComponent } from './chapter-seven/chapter-seven.component';
 import { ChapterNineComponent } from './chapter-nine/chapter-nine.component';
-import { ChapterTenComponent } from './chapter-ten/chapter-ten.component';
 import { ChapterFourteenComponent } from './chapter-fourteen/chapter-fourteen.component';
 import { ChapterFifteenComponent } from './chapter-fifteen/chapter-fifteen.component';
 
@@ -31,13 +26,8 @@ import { ChapterFifteenComponent } from './chapter-fifteen/chapter-fifteen.compo
     SearchComponent,
     FormatSearchPipe,
     ChapterOneComponent,
-    ChapterTwoComponent,
-    ChapterThreeComponent,
-    ChapterFiveComponent,
     ChapterSixComponent,
-    ChapterSevenComponent,
     ChapterNineComponent,
-    ChapterTenComponent,
     ChapterFourteenComponent,
     ChapterFifteenComponent
   ],
