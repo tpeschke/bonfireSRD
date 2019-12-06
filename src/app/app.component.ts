386c755a4021f4f71b35ce0e2ee5e8d2aed21d6d
Trent <mr.peschke@gmail.com>
2019-12-06 14:38:59 -0700

    got page transitions to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NotReduxService } from './not-redux.service';
 import { Router } from '@angular/router'
+import { fadeInAnimation } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [fadeInAnimation]
 })
 export class AppComponent {
   constructor(
