a02a403cd91bfbe175ad2dd52d88fae04f5ae7b9
Trent <mr.peschke@gmail.com>
2020-02-05 12:16:09 -0700

    fixing the discord banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { NotReduxService } from '../not-redux.service';
 
 @Component({
@@ -6,11 +6,20 @@ import { NotReduxService } from '../not-redux.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewChecked {
 
   constructor(
-    private notRedux: NotReduxService) { }
+    private notRedux: NotReduxService
+  ) { }
 
-  ngOnInit() {}
+  private displayBanner = false;
 
-}
+  ngOnInit() {
+    
+  }
+
+  ngAfterViewChecked() {
+    this.displayBanner = true;
+  }
+
+} 
