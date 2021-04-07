2ee113c428c43343584b8d6fc1a1b744a0c5469d
Trent <mr.peschke@gmail.com>
2021-04-07 11:19:04 -0600

    Patch 4.17.13

--- src/app/chapters/chapter-fourteen/chapter-fourteen.component.ts
+++ src/app/chapters/chapter-fourteen/chapter-fourteen.component.ts
@@ -13,7 +13,7 @@ export class ChapterFourteenComponent implements OnInit {
   ) { }
 
   chapterName = '';
-  side =[{linkid: 'hnu', body: 'Gaining Experience', jump: '14hzdDOqjnAgZ'}, {linkid: 'h', body: 'Leveling Up', jump: '14hZqkalOSwRr'}, {linkid: 'hg', body: 'DAY 1: RECEIVE CRP', jump: '14hghilHrGsooj'}, {linkid: 'hg', body: 'DAY 2: INCREASE STAT SCORES', jump: '14hgNgyrqXbebx'}, {linkid: 'hg', body: 'DAY 3: ASSESS PERSONALITY ', jump: '14hgtAZiWwZJoy'}, {linkid: 'hg', body: 'DAY 4: INCREASE ARCHETYPE ABILITIES', jump: '14hgePIgBrVjYz'}, {linkid: 'hg', body: 'DAY 5: TRAIN AND RETRAIN SKILLS & TRAININGS ', jump: '14hgbtjUfRlAsf'}, {linkid: 'hg', body: 'DAY 6: INCREASE VITALITY', jump: '14hgBavRZYfJAF'}, {linkid: 'hg', body: 'DAY 7: ADJUST HONOR & REPUTATION', jump: '14hgitOLfeiEUO'},  {linkid: 'h', body: 'Attracting NPCs', jump: 'ATTRACTINGNPCuydmDjGgOm'}]
+  side =[{linkid: 'hnu', body: 'Gaining Experience', jump: '14hzdDOqjnAgZ'}, {linkid: 'h', body: 'Leveling Up', jump: '14hZqkalOSwRr'}, {linkid: 'hg', body: 'DAY 1: RECEIVE CRP', jump: '14hghilHrGsooj'}, {linkid: 'hg', body: 'DAY 2: INCREASE STAT SCORES', jump: '14hgNgyrqXbebx'}, {linkid: 'hg', body: 'DAY 3: ASSESS PERSONALITY ', jump: '14hgtAZiWwZJoy'}, {linkid: 'hg', body: 'DAY 4: INCREASE ARCHETYPE ABILITIES', jump: '14hgePIgBrVjYz'}, {linkid: 'hg', body: 'DAY 5: TRAIN AND RETRAIN SKILLS & TRAININGS ', jump: '14hgbtjUfRlAsf'}, {linkid: 'hg', body: 'DAY 6: INCREASE VITALITY & STRESS THRESHOLD', jump: '14hgBavRZYfJAF'}, {linkid: 'hg', body: 'DAY 7: ADJUST HONOR & REPUTATION', jump: '14hgitOLfeiEUO'},  {linkid: 'h', body: 'Attracting NPCs', jump: 'ATTRACTINGNPCuydmDjGgOm'}]
 
   ngOnInit(){}
 
