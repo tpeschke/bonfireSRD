f269b06ac78487f118d0640d9d11f2f7df35472f
Trent <mr.peschke@gmail.com>
2021-12-27 17:36:30 -0700

    pausing because I love my girlfriend

--- src/app/character-creation/chapter-six/cc-six/cc-six.component.ts
+++ src/app/character-creation/chapter-six/cc-six/cc-six.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import tables from '../tables'
 
 @Component({
   selector: 'app-cc-six',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class CcSixComponent implements OnInit {
 
   constructor() { }
+
+  tables = tables.tables;
+  multi = tables.multipliers
 
   ngOnInit() {
   }
