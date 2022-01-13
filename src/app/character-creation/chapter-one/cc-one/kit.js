7f3f3bf396e662bc88e6eed9e3a07e80b48a3097
Trent <mr.peschke@gmail.com>
2022-01-13 07:55:05 -0700

    Patch 4.18.4.2

--- src/app/character-creation/chapter-one/cc-one/kit.js
+++ src/app/character-creation/chapter-one/cc-one/kit.js
@@ -2,548 +2,548 @@ export default [
     {
         name: "Adventurer's Kit",
         weight: "1L 2M",
-        cash: "4d20!",
+        cash: "4d20! sc",
         items: [
             {
                 name: 'Sheathe, Small, Leather x3*',
                 size: 'n/a',
-                sellback: '7.5'
+                sellback: '7.5 sc'
             },
             {
                 name: '2 S Weapons*',
                 size: 'n/a',
-                sellback: '30'
+                sellback: '30 sc'
             },
             {
                 name: '3 Thrown Ranged Weapons*',
                 size: 'n/a',
-                sellback: '15 per'
+                sellback: '15 sc per'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Backpack, Large*',
                 size: '+1L',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Rations, Person, Day x7',
                 size: '7S',
-                sellback: '17.5'
+                sellback: '17.5 sc'
             },
             {
                 name: 'Beeswax',
                 size: 'S',
-                sellback: '0.3'
+                sellback: '0.3 sc'
             },
             {
                 name: 'Fabric, Canvas, Soft (Sq. Yard)',
                 size: 'S',
-                sellback: '2.6'
+                sellback: '2.6 sc'
             },
             {
                 name: 'Rope, Hemp, 1/2" (250#), 100 ft',
                 size: 'M',
-                sellback: '26'
+                sellback: '26 sc'
             },
             {
                 name: 'Auger, Wood Handle, Steel Head',
                 size: 'M',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Chalk, 4", Box of 20',
                 size: 'S',
-                sellback: '.05'
+                sellback: '.05 sc'
             },
             {
                 name: 'Grapnel Hook',
                 size: 'S',
-                sellback: '16'
+                sellback: '16 sc'
             },
             {
                 name: 'Mallet, Wood',
                 size: 'S',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Spike, Iron, 12"',
                 size: 'S',
-                sellback: '1.25'
+                sellback: '1.25 sc'
             },
             {
                 name: 'Torch, Tallow x5',
                 size: '5S',
-                sellback: '1'
+                sellback: '1 sc'
             }
         ]
     },
     {
         name: "Merchant's Kit",
         weight: "n/a (1L 1S on mule)",
-        cash: "5d20!",
+        cash: "5d20! sc",
         items: [
             {
                 name: 'Sheathe, Small, Leather*',
                 size: 'n/a',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: '1 S Weapon*',
                 size: 'n/a',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Mule',
                 size: 'n/a',
-                sellback: '90'
+                sellback: '90 sc'
             },
             {
                 name: 'Rations, Person, Day x7',
                 size: '7S',
-                sellback: '17.5'
+                sellback: '17.5 sc'
             },
             {
                 name: 'Canteen (Quart)',
                 size: 'S',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Bag, Small',
                 size: 'S',
-                sellback: '024'
+                sellback: '0.24 sc'
             },
             {
                 name: 'Rations, Dairy Cow, Hay, Daily x7',
                 size: '7M',
-                sellback: '1.61'
+                sellback: '1.61 sc'
             }
         ]
     },
     {
         name: "Traveler's Kit",
         weight: "1M 2S",
-        cash: "2d10!",
+        cash: "2d10! sc",
         items: [
             {
                 name: 'Coat of Plates*',
                 size: 'S',
-                sellback: '190'
+                sellback: '190 sc'
             },
             {
                 name: 'Sheathe, Small, Leather*',
                 size: 'n/a',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: '1 S Weapons*',
                 size: 'n/a',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Coat, Buckskin*',
                 size: 'S',
-                sellback: '50'
+                sellback: '50 sc'
             },
             {
                 name: 'Hat, Very Large, Rabbit Fur*',
                 size: 'n/a',
-                sellback: '.15'
+                sellback: '.15 sc'
             },
             {
                 name: 'Rations, Person, Day x3',
                 size: '3S',
-                sellback: '7.5'
+                sellback: '7.5 sc'
             },
         ]
     },
     {
         name: "Thief's Kit",
         weight: "1M",
-        cash: "5d20!",
+        cash: "5d20! sc",
         items: [
             {
                 name: 'Sheathe, Small, Leather x2*',
                 size: 'n/a',
-                sellback: '5'
+                sellback: '5 sc'
             },
             {
                 name: '2 S Weapons*',
                 size: 'n/a',
-                sellback: '30'
+                sellback: '30 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Backpack, Large*',
                 size: '+1L',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Rations, Person, Day x1',
                 size: 'S',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: 'Beeswax',
                 size: 'S',
-                sellback: '0.3'
+                sellback: '0.3 sc'
             },
             {
                 name: 'Rope, Hemp, 1/2" (250#), 100 ft',
                 size: 'M',
-                sellback: '26'
+                sellback: '26 sc'
             },
             {
                 name: 'Chalk, 4", Box of 20',
                 size: 'S',
-                sellback: '.05'
+                sellback: '.05 sc'
             },
             {
                 name: 'Grapnel Hook',
                 size: 'S',
-                sellback: '16'
+                sellback: '16 sc'
             },
             {
                 name: 'Spike, Iron, 12"',
                 size: 'S',
-                sellback: '1.25'
+                sellback: '1.25 sc'
             },
             {
                 name: 'Bag, Medium x3',
                 size: '3S',
-                sellback: '19.6'
+                sellback: '19.6 sc'
             },
             {
                 name: 'Lock Picks',
                 size: 'S',
-                sellback: '12'
+                sellback: '12 sc'
             }
         ]
     },
     {
         name: "Basic Fighters's Kit",
         weight: "1M or 2M",
-        cash: "2d20!",
+        cash: "2d20! sc",
         items: [
             {
                 name: 'Buff Coat*',
                 size: 'n/a',
-                sellback: '100'
+                sellback: '100 sc'
             },
             {
                 name: 'Scabbard*',
                 size: 'n/a',
-                sellback: '5'
+                sellback: '5 sc'
             },
             {
                 name: '1 L Weapon or 1 M Weapon & 3 Thrown Ranged Weapons*',
                 size: 'M or n/a',
-                sellback: '100'
+                sellback: '100 sc'
             },
             {
                 name: 'Sheathe, Small*',
                 size: 'n/a',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: '1 S Weapon*',
                 size: 'n/a',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Rations, Person, Day x3',
                 size: '3S',
-                sellback: '7.5'
+                sellback: '7.5 sc'
             },
         ]
     },
     {
         name: "Armor Fighter's Kit",
         weight: "1M 3S",
-        cash: "2d10!",
+        cash: "2d10! sc",
         items: [
             {
                 name: 'Coat of Plates*',
                 size: 'S',
-                sellback: '190'
+                sellback: '190 sc'
             },
             {
                 name: 'Scabbard*',
                 size: 'n/a',
-                sellback: '5'
+                sellback: '5 sc'
             },
             {
                 name: '1 M Weapon*',
                 size: 'S',
-                sellback: '50'
+                sellback: '50 sc'
             },
             {
                 name: 'Sheathe, Small*',
                 size: 'n/a',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: '1 S Weapon*',
                 size: 'n/a',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Rations, Person, Day x3',
                 size: '3S',
-                sellback: '7.5'
+                sellback: '7.5 sc'
             },
         ]
     },
     {
         name: "Shield Fighter's Kit",
         weight: "1L 1M 1S",
-        cash: "4d20!",
+        cash: "4d20! sc",
         items: [
             {
                 name: 'Heater Shield',
                 size: 'L',
-                sellback: '60'
+                sellback: '60 sc'
             },
             {
                 name: 'Buff Coat*',
                 size: 'n/a',
-                sellback: '100'
+                sellback: '100 sc'
             },
             {
                 name: 'Scabbard*',
                 size: 'n/a',
-                sellback: '5'
+                sellback: '5 sc'
             },
             {
                 name: '1 M Weapon*',
                 size: 'S',
-                sellback: '50'
+                sellback: '50 sc'
             },
             {
                 name: 'Sheathe, Small*',
                 size: 'n/a',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: '1 S Weapon*',
                 size: 'n/a',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Rations, Person, Day x3',
                 size: '3S',
-                sellback: '7.5'
+                sellback: '7.5 sc'
             },
         ]
     },
     {
         name: "Long Range Fighter's Kit",
         weight: "1M 1S or 1M",
-        cash: "4d10!",
+        cash: "4d10! sc",
         items: [
             {
                 name: 'Buff Coat*',
                 size: 'n/a',
-                sellback: '100'
+                sellback: '100 sc'
             },
             {
                 name: 'Scabbard*',
                 size: 'n/a',
-                sellback: '5'
+                sellback: '5 sc'
             },
             {
                 name: '1 M or S Ranged Weapon*',
                 size: 'S',
-                sellback: '50'
+                sellback: '50 sc'
             },
             {
                 name: 'Sheathe, Small*',
                 size: 'n/a',
-                sellback: '2.5'
+                sellback: '2.5 sc'
             },
             {
                 name: '1 S Weapon*',
                 size: 'n/a',
-                sellback: '15'
+                sellback: '15 sc'
             },
             {
                 name: 'Quiver, 12 Cap**',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Ranged Ammunition x12*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Belt, Leather*',
                 size: 'n/a',
-                sellback: '1'
+                sellback: '1 sc'
             },
             {
                 name: 'Breeches, Linen*',
                 size: 'n/a',
-                sellback: '12'
+                sellback: '12 sc'
             },
             {
                 name: 'Shirt, Wool*',
                 size: 'n/a',
-                sellback: '13'
+                sellback: '13 sc'
             },
             {
                 name: 'Boots, Leather*',
                 size: 'n/a',
-                sellback: '10'
+                sellback: '10 sc'
             },
             {
                 name: 'Rations, Person, Day x3',
                 size: '3S',
-                sellback: '7.5'
+                sellback: '7.5 sc'
             },
         ]
     },
     {
         name: "Blank Slate Kit",
         weight: "n/a",
-        cash: "5d20! + 275",
+        cash: "5d20! + 275 sc",
         items: [
             {
                 name: 'None',
