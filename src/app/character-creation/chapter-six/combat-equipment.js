d82eb81b3f8f54c028ed72182ba111193471e6cf
Trent <mr.peschke@gmail.com>
2022-04-30 12:05:01 -0600

    Patch 4.18.8.3

---
+++ src/app/character-creation/chapter-six/combat-equipment.js
@@ -0,0 +1,205 @@
+export default {
+    armor: [
+        {
+            name: 'Buff Coat',
+            dr: '2',
+            def: 0,
+            init: 0,
+            rec: 2,
+            fatigue: -1,
+            skill: 0,
+        },
+        {
+            name: 'Gambeson',
+            dr: '3',
+            def: 0,
+            init: 0,
+            rec: 0,
+            fatigue: -1,
+            skill: -2
+        },
+        {
+            name: 'Leather',
+            dr: '4',
+            def: -1,
+            init: 1,
+            rec: 1,
+            fatigue: -1,
+            skill: -2
+        },
+        {
+            name: 'Coat of Plates',
+            dr: '2/d',
+            def: -4,
+            init: 0,
+            rec: 5,
+            fatigue: -2,
+            skill: -2
+        },
+        {
+            name: 'Lamellar',
+            dr: '3/d',
+            def: -4,
+            init: 5,
+            rec: 0,
+            fatigue: -3,
+            skill: -4
+        },
+        {
+            name: 'Brigandine',
+            dr: '3/d',
+            def: -5,
+            init: 5,
+            rec: 3,
+            fatigue: -1,
+            skill: -4
+        },
+        {
+            name: 'Laminar (Banded Mail)',
+            dr: '3/d',
+            def: 0,
+            init: 5,
+            rec: 4,
+            fatigue: -3,
+            skill: -4
+        },
+        {
+            name: 'Chainmail',
+            dr: '3/d',
+            def: -3,
+            init: 3,
+            rec: 3,
+            fatigue: -3,
+            skill: -4
+        },
+        {
+            name: 'Plated Mail',
+            dr: '2/d + 6',
+            def: -6,
+            init: 6,
+            rec: 6,
+            fatigue: -6,
+            skill: -6
+        },
+        {
+            name: 'Scale',
+            dr: '4/d + 2',
+            def: -8,
+            init: 7,
+            rec: 5,
+            fatigue: -5,
+            skill: -8
+        },
+        {
+            name: 'Full Plate',
+            dr: '6/d + 7',
+            def: -10,
+            init: 10,
+            rec: 9,
+            fatigue: -8,
+            skill: -8
+        }
+    ],
+    shields: [
+        {
+            name: 'Buckler',
+            dr: '6/d + 4',
+            def: -1,
+            parry: 5,
+            cover: '1',
+            flanks: 2,
+            fatigueigue: -4,
+            bonus: 'Yes'
+        },
+        {
+            name: 'Clothe',
+            dr: '6',
+            def: 0,
+            parry: 5,
+            cover: '0',
+            flanks: 2,
+            fatigueigue: 0,
+            bonus: ''
+        },
+        {
+            name: 'Figure Eight',
+            dr: '4/d + 4',
+            def: -4,
+            parry: 12,
+            cover: 'd6!+4',
+            flanks: 3,
+            fatigueigue: -3,
+            bonus: 'Yes'
+        },
+        {
+            name: 'Heater',
+            dr: '3/d + 6',
+            def: -6,
+            parry: 15,
+            cover: 'd8!+4',
+            flanks: 3,
+            fatigueigue: -2,
+            bonus: ''
+        },
+        {
+            name: 'Hoplon',
+            dr: '4/d + 4',
+            def: -3,
+            parry: 9,
+            cover: 'd8!+7',
+            flanks: 3,
+            fatigueigue: -1,
+            bonus: ''
+        },
+        {
+            name: 'Kite',
+            dr: '3/d + 2',
+            def: -8,
+            parry: 14,
+            cover: '2d6!+8',
+            flanks: 4,
+            fatigueigue: -2,
+            bonus: ''
+        },
+        {
+            name: 'Pavise',
+            dr: '4/d + 2',
+            def: -8,
+            parry: 0,
+            cover: 'd20!+8',
+            flanks: 1,
+            fatigueigue: -5,
+            bonus: 'Yes'
+        },
+        {
+            name: 'Round',
+            dr: '3/d + 2',
+            def: -4,
+            parry: 11,
+            cover: 'd12!+6',
+            flanks: 3,
+            fatigueigue: -3,
+            bonus: 'Yes'
+        },
+        {
+            name: 'Tower',
+            dr: '2/d + 1',
+            def: -10,
+            parry: 18,
+            cover: '2d8!+10',
+            flanks: 3,
+            fatigueigue: -2,
+            bonus: ''
+        }
+    ],
+    weapons: {
+        axes: [],
+        polearms: [],
+        sidearms: [],
+        swords: [],
+        trauma: [],
+        thrown: [],
+        mechanical: [],
+        firearms: []
+    }
+}
