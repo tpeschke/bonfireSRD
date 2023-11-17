5bceabcbda6ef69c724846a2660e242fa0cae88c
Trent <mr.peschke@gmail.com>
2023-11-16 18:09:42 -0700

    bug squashing

--- src/app/character-creation/chapter-four/burdenAndInjuriesTable/burdenAndInjuriesTable.component.ts
+++ src/app/character-creation/chapter-four/burdenAndInjuriesTable/burdenAndInjuriesTable.component.ts
@@ -6,17 +6,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./burdenAndInjuriesTable.component.css', '../cc-four/cc-four.component.css', '../../../chapter.css']
 })
 export class BurdenAndInjuriesTableComponent implements OnInit {
-  @Input() flawTable: any; 
+  @Input() ibTable: any; 
   @Input() label: any;
 
   constructor() { }
 
   ngOnInit() {
     let currentTotal = 0
-    this.flawTable = this.flawTable.map(flaw => {
-      const flawWithCurrentTotal = {...flaw, currentTotal}
-      currentTotal += flaw.weight
-      return flawWithCurrentTotal
+    this.ibTable = this.ibTable.map(ib => {
+      const ibWithCurrentTotal = {...ib, currentTotal}
+      currentTotal += ib.weight
+      return ibWithCurrentTotal
     })
   }
   
