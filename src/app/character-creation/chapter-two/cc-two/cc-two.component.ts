e9bf4589304cfff5d3b03caca3f4698ca823425b
Trent <mr.peschke@gmail.com>
2024-09-12 18:20:01 -0600

    Patch 4.20.2.5

--- src/app/character-creation/chapter-two/cc-two/cc-two.component.ts
+++ src/app/character-creation/chapter-two/cc-two/cc-two.component.ts
@@ -12,7 +12,7 @@ export class CcTwoComponent implements OnInit {
     private chapterService: ChapterService,
   ) { }
 
-  side = [{linkid: 'h', body: 'Which Ancestry Should I Choose?', jump: '3ibTsjRfDjl'}, {linkid: 'hg', body: 'Stat Score Modifiers', jump: '3kOpuEvckQq'}, {linkid: 'hg', body: 'Gameplay Rewards', jump: '3FtaYQizyom'}, {linkid: 'h', body: 'Elves', jump: '3h1'}, {linkid: 'hg', body: 'Appearance', jump: '3hg16'}, {linkid: 'hg', body: 'Personality', jump: '3hg17'}, {linkid: 'h', body: 'Humans', jump: '3h2'}, {linkid: 'hg', body: 'Class Costs', jump: '3hg9'}, {linkid: 'hg', body: 'Appearance', jump: '3hg19'}, {linkid: 'hg', body: 'Personality', jump: '3hg20'}, {linkid: 'h', body: 'Orcs', jump: 'orcheadertag'}, {linkid: 'hg', body: 'Appearance', jump: '3hg22'}, {linkid: 'hg', body: 'Personality', jump: '3hg23'}]
+  side = [{linkid: 'h', body: 'Which Ancestry Should I Choose?', jump: '3ibTsjRfDjl'}, {linkid: 'hg', body: 'Stat Score Modifiers', jump: '3kOpuEvckQq'}, {linkid: 'hg', body: 'Gameplay Rewards', jump: '3FtaYQizyom'}, {linkid: 'hg', body: 'Class Costs', jump: '3NLyIsVRDgl'}, {linkid: 'h', body: 'Elves', jump: '3h1'}, {linkid: 'hg', body: 'Appearance', jump: '3hg16'}, {linkid: 'hg', body: 'Personality', jump: '3hg17'}, {linkid: 'h', body: 'Humans', jump: '3h2'}, {linkid: 'hg', body: 'Appearance', jump: '3hg19'}, {linkid: 'hg', body: 'Personality', jump: '3hg20'}, {linkid: 'h', body: 'Orcs', jump: 'orcheadertag'}, {linkid: 'hg', body: 'Appearance', jump: '3hg22'}, {linkid: 'hg', body: 'Personality', jump: '3hg23'}]
   characterisitics = {}
 
   ngOnInit() {
